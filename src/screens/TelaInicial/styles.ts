e2ff8eaf42f46ab64338073385ef2496d1d14e88
fabiobs30 <fabiobarrossena30@gmail.com>
2024-04-21 16:48:55 -0300

    car

--- src/screens/TelaInicial/styles.ts
+++ src/screens/TelaInicial/styles.ts
@@ -1,3 +1,4 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 
@@ -5,4 +6,13 @@ export const Container = styled.View`
     flex: 1;
     background-color: ${({theme})=> theme.colors.primary};
     
+`;
+export const Body = styled.View`
+  justify-content: center;
+  height: ${RFValue(300)}px;
+`;
+export const Title = styled.Text`
+  font-size: ${RFValue(25)}px;
+  text-align: center;
+  color: ${({theme})=> theme.colors.colorWhite};
 `;
